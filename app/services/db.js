7b8360bcceb06bf857f8aba25fcb77a8b9339da3
Wisnu Wijokangko <wisnuwijo33@gmail.com>
2021-09-14 11:32:09 +0700

    update cors middleware and add error handler on connection service

--- app/services/db.js
+++ app/services/db.js
@@ -4,20 +4,35 @@ import mysql from 'mysql';
 // get config vars
 dotenv.config();
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+var connection;
 
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+function connectToDB() {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
 
-    console.log('connected as id ' + connection.threadId);
-});
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            setTimeout(connectToDB(), 2000);
+        }
+    
+        console.log('connected as id ' + connection.threadId);
+        connection.on('error', err => {
+            console.log('ERROR ', err);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                // db error reconnect
+                connectToDB();
+            } else {
+                throw err;
+            }
+        });
+    });
+}
+
+connectToDB();
 
 export default connection;
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const corsOptions = {
     optionsSuccessStatus: 200
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
